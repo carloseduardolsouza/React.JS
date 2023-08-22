b58cd6abb5894f00c62e7d9dbdb4b908dfb645a9
Carlos Eduardo Lourenço De Souza <97536308+carloseduardolsouza@users.noreply.github.com>
2023-08-22 14:41:58 -0300

    aplicaçao react

---
+++ teste_conhecimentos/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./style/EstiloNavBar.css"
+
+function NavBar() {
+    return (
+    <nav>
+        <img src="#" alt="logo" />
+        <ul>
+            <li>Inicial</li>
+            <li>Novo pedido</li>
+            <li>Consultar pedido</li>
+        </ul>
+    </nav>
+    )
+}
+
+export default NavBar
